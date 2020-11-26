8dda2f392dd8400c9db29471524ad8760d2d814e
Phan Xuan Phuong <pxphuong94@gmail.com>
2020-11-26 17:35:41 +0100

    Committing the continuation of the Drawing Workshop of w02d03-04, still a couple left to go

--- week-02/day-3/src/RainbowBoxFunction.java
+++ week-02/day-3/src/RainbowBoxFunction.java
@@ -11,35 +11,35 @@ public class RainbowBoxFunction {
         // and draws a square of that size and color to the center of the canvas.
         // Create a loop that fills the canvas with rainbow colored squares (red, orange, yellow, green, blue, indigo, violet).
 
-        Color myIndigo =  new Color(29, 0, 51);
+        Color myIndigo = new Color(29, 0, 51);
         Color myViolet = new Color(93, 51, 93);
         Color[] fillColor = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, myIndigo, myViolet};
 
         int i;
-        int amountOfBoxes = 40;
-        int lineDistance = WIDTH / amountOfBoxes;
-        for (i = 0; i < amountOfBoxes; i++) {
-            drawSquare(lineDistance/2, fillColor[(int)(Math.random()*fillColor.length)], graphics);
+        int amountOfBoxes = 10;
+        int sideSize = WIDTH / amountOfBoxes;
+        for (i = amountOfBoxes; i > 0; i--) {
+            drawSquare(sideSize * i, fillColor[(int) (Math.random() * fillColor.length)], graphics);
         }
-
 
 
     }
 
     public static void drawSquare(Integer s, Color color, Graphics g) {
         g.setColor(color);
-        g.drawLine(W-s, H+s, W+s, H+s );
-        g.drawLine(W+s, H+s, W+s, H-s );
-        g.drawLine(W+s, H-s, W-s, H-s );
-        g.drawLine(W-s, H-s, W-s, H+s );
-        g.fillRect();
+             /*   g.drawLine(W-s, H+s, W+s, H+s );
+                g.drawLine(W+s, H+s, W+s, H-s );
+                g.drawLine(W+s, H-s, W-s, H-s );
+                g.drawLine(W-s, H-s, W-s, H+s );
+                g.fillRect(); */
+        g.fillRect(W - s/2, H - s/2, s, s);
     }
 
     // Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 320;
-    static int W = WIDTH/2;
-    static int H = HEIGHT/2;
+    static int W = WIDTH / 2;
+    static int H = HEIGHT / 2;
 
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Drawing");
