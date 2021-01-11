d209b3c8c9d62b46f9c00eaa69ccc4c869aae9e0
Phan Xuan Phuong <pxphuong94@gmail.com>
2021-01-11 17:38:20 +0100

    added optional features progress

--- week-06/day-5/src/main/java/com/greenfoxacademy/basicwebshop/controllers/MainController.java
+++ week-06/day-5/src/main/java/com/greenfoxacademy/basicwebshop/controllers/MainController.java
@@ -13,22 +13,23 @@ public class MainController {
 
     List<ShopItem> shopItemList = new ArrayList<>();
 
-    public MainController(){
-        shopItemList.add(new ShopItem("Running shoes", "Nike running shoes for every day sport", 1000.0, 5));
-        shopItemList.add(new ShopItem("Printer", "Some HP printer that will print pages", 3000.0, 2));
-        shopItemList.add(new ShopItem("Coca cola", "0.5l standard coke", 25.0, 0));
-        shopItemList.add(new ShopItem("Wokin", "Chicken with fried rice and WOKIN sauce", 119.0, 100));
-        shopItemList.add(new ShopItem("T-Shirt", "Blue with a corgi on a bike", 300.0, 1));
+    public MainController() {
+        shopItemList.add(new ShopItem("Running shoes", "Nike running shoes for every day sport", 1000.0, 5, "Clothes and Shoes"));
+        shopItemList.add(new ShopItem("Printer", "Some HP printer that will print pages", 3000.0, 2, "Electronics"));
+        shopItemList.add(new ShopItem("Coca cola", "0.5l standard coke", 25.0, 0, "Beverages and Snacks"));
+        shopItemList.add(new ShopItem("Wokin", "Chicken with fried rice and WOKIN sauce", 119.0, 100, "Beverages and Snacks"));
+        shopItemList.add(new ShopItem("T-Shirt", "Blue with a corgi on a bike", 300.0, 1, "Clothes and shoes"));
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/webshop")
-    public String webShop(Model model){
+    public String webShop(Model model) {
         model.addAttribute("shopItemList", shopItemList);
         return "index";
     }
 
+
     @GetMapping("/only-available")
-    public String onlyAvailable(Model model){
+    public String onlyAvailable(Model model) {
         List<ShopItem> onlyAvailableList = shopItemList.stream()
                 .filter(x -> x.getQuantityOfStock() != 0)
                 .collect(Collectors.toList());
@@ -37,7 +38,7 @@ public class MainController {
     }
 
     @GetMapping("/cheapest-first")
-    public String cheapestFirst(Model model){
+    public String cheapestFirst(Model model) {
         List<ShopItem> cheapestFirstList = shopItemList.stream()
                 .sorted(Comparator.comparing(ShopItem::getPrice))
                 .collect(Collectors.toList());
@@ -46,14 +47,14 @@ public class MainController {
     }
 
     @GetMapping("/most-expensive")
-    public String mostExpensive(Model model){
+    public String mostExpensive(Model model) {
         Optional<ShopItem> optionalMostExpensiveShopItem = shopItemList.stream()
-                .filter(x -> x.getQuantityOfStock()>0)
+                .filter(x -> x.getQuantityOfStock() > 0)
                 .sorted(Comparator.comparing(ShopItem::getPrice).reversed())
                 .findFirst();
 
         String mostExpensiveName;
-        if (optionalMostExpensiveShopItem.isPresent()){
+        if (optionalMostExpensiveShopItem.isPresent()) {
             mostExpensiveName = optionalMostExpensiveShopItem.get().getName();
         } else {
             mostExpensiveName = "None.";
@@ -64,7 +65,7 @@ public class MainController {
     }
 
     @GetMapping("/contains-nike")
-    public String containsNike(Model model){
+    public String containsNike(Model model) {
         List<ShopItem> containsNikeList = shopItemList.stream()
                 .filter(x -> x.getDescription().contains("Nike"))
                 .collect(Collectors.toList());
@@ -72,34 +73,34 @@ public class MainController {
         return "index";
     }
 
-  //  @GetMapping("average-stock")
-  //  public String averageStock (Model model)
-  //      List<ShopItem> averageStockList = shopItemList.stream()
-  //              .mapToInt(ShopItem::getQuantityOfStock)
-  //              .sum()
+    //  @GetMapping("average-stock")
+    //  public String averageStock (Model model)
+    //      List<ShopItem> averageStockList = shopItemList.stream()
+    //              .mapToInt(ShopItem::getQuantityOfStock)
+    //              .sum()
 
     //  a reset gombot a sima webShop()-ra!
 
     @GetMapping("average-stock")
-    public String averageStock(Model model){
+    public String averageStock(Model model) {
         OptionalDouble optionalDouble = shopItemList.stream()
-               .mapToInt(ShopItem::getQuantityOfStock)
-               .average();
+                .mapToInt(ShopItem::getQuantityOfStock)
+                .average();
 
         Double averageStock;
 
-        if (optionalDouble.isPresent()){
+        if (optionalDouble.isPresent()) {
             averageStock = optionalDouble.getAsDouble();
         } else {
             averageStock = 0D;
         }
 
-       model.addAttribute("averageStock", averageStock);
+        model.addAttribute("averageStock", averageStock);
         return "average-stock";
     }
 
     @PostMapping("/search")
-    public String search(Model model, @RequestParam (required = false) String searchId){
+    public String search(Model model, @RequestParam(required = false) String searchId) {
         List<ShopItem> foundShopItemList = shopItemList.stream()
                 .filter(x -> x.getName().contains(searchId) || x.getDescription().contains(searchId))
                 .collect(Collectors.toList());
@@ -107,10 +108,36 @@ public class MainController {
         return "index";
     }
 
+    private List<ShopItem> filter(String name){
+        List<ShopItem> filteredByTypeList = shopItemList.stream()
+                .filter(x -> x.getType().equals(name))
+                .collect(Collectors.toList());
+        return filteredByTypeList;
+    }
+
     @GetMapping("/")
-    public String noPath(){
+    public String noPath() {
         return "redirect:/webshop";
     }
+
+    @GetMapping("/more-filters")
+    public String moreFilters(Model model) {
+        model.addAttribute("shopItemList", shopItemList);
+        return "type-index";
+    }
+
+    @GetMapping("/filtered-by-type/{name}")
+    public String filterByType(Model model, @PathVariable String name){
+        List<ShopItem> filteredByTypeList = filter(name);
+
+        model.addAttribute("shopItemList", filteredByTypeList);
+        model.addAttribute("name", name);
+        return "type-index";
+    }
+
+   // @GetMapping("/filtered-by-type/{type}")
+   // public String filterByProductType (@PathVariable String )
+
 
 
 
--- week-06/day-5/src/main/java/com/greenfoxacademy/basicwebshop/models/ShopItem.java
+++ week-06/day-5/src/main/java/com/greenfoxacademy/basicwebshop/models/ShopItem.java
@@ -6,12 +6,28 @@ public class ShopItem {
     private String description;
     private Double price;
     private Integer quantityOfStock;
+    private String type;
+    private String currency = "Ft";
 
-    public ShopItem(String name, String description, Double price, Integer quantityOfStock) {
+
+    public ShopItem(String name, String description, Double price, Integer quantityOfStock, String type) {
         this.name = name;
         this.description = description;
         this.price = price;
         this.quantityOfStock = quantityOfStock;
+        this.type = type;
+    }
+
+    public String getFormattedPrice() {
+        return getPrice().toString() + " " + getCurrency();
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getType() {
+        return type;
     }
 
     public String getName() {
@@ -28,4 +44,5 @@ public class ShopItem {
 
     public Integer getQuantityOfStock() {
         return quantityOfStock;
-    }}
+    }
+}
