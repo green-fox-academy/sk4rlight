9a02b34557ebb11605220d0a0c701e4169d87cf7
Phan Xuan Phuong <pxphuong94@gmail.com>
2021-02-18 16:42:02 +0100

    added JWT auth

--- project-week-00/src/main/java/com/greenfoxacademy/springadvanced/controller/MainController.java
+++ project-week-00/src/main/java/com/greenfoxacademy/springadvanced/controller/MainController.java
@@ -1,15 +1,23 @@
 package com.greenfoxacademy.springadvanced.controller;
 
+import com.greenfoxacademy.springadvanced.model.AuthenticationRequest;
+import com.greenfoxacademy.springadvanced.model.AuthenticationResponse;
 import com.greenfoxacademy.springadvanced.model.Movie;
+import com.greenfoxacademy.springadvanced.model.MyUserDetails;
 import com.greenfoxacademy.springadvanced.service.MovieService;
-import com.greenfoxacademy.springadvanced.service.TheMovieDBApiServiceGenerator;
+import com.greenfoxacademy.springadvanced.service.MyUserDetailsService;
+import com.greenfoxacademy.springadvanced.utility.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import sun.reflect.annotation.ExceptionProxy;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +28,17 @@ public class MainController {
     @Autowired
     MovieService movieService;
 
+    // override in SecurityConfiguration authenticationManagerBean(){}
+    // and @Bean that method
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
     @GetMapping("/movie/{id}")
     public ResponseEntity<Movie> getMovie(@PathVariable Integer id) {
 
@@ -29,26 +48,50 @@ public class MainController {
     }
 
     @GetMapping("/movie/popular")
-    public List<Movie> getPopularMovies(){
+    public List<Movie> getPopularMovies() {
         List<Movie> popularMovieList = new ArrayList<>();
         return popularMovieList;
     }
 
     @ResponseBody
     @GetMapping({"/", "/home"})
-    public String home(){
+    public String home() {
         return ("<h1>Welcome home");
     }
 
     @ResponseBody
     @GetMapping("/user")
-    public String user(){
+    public String user() {
         return ("<h1>Welcome User");
     }
 
     @ResponseBody
     @GetMapping("/admin")
-    public String admin(){
+    public String admin() {
         return ("<h1>Welcome Admin");
+    }
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+        final UserDetails userDetails = myUserDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+        return new ResponseEntity<>(new AuthenticationResponse(jwt), HttpStatus.OK);
     }
 }
--- project-week-00/src/main/java/com/greenfoxacademy/springadvanced/security/SpringSecurityConfiguration.java
+++ project-week-00/src/main/java/com/greenfoxacademy/springadvanced/security/SpringSecurityConfiguration.java
@@ -1,24 +1,32 @@
 package com.greenfoxacademy.springadvanced.security;
 
+import com.greenfoxacademy.springadvanced.filter.JwtRequestFilter;
 import com.greenfoxacademy.springadvanced.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -35,18 +43,35 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(myUserDetailsService);
     }
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+    /*    http.authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
                 .antMatchers("/user").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/**").hasRole("ADMIN")
                 .antMatchers("/admin").hasRole("ADMIN")
                 .and().formLogin();
+
+     */
+        // for this particular API "/authenticate" let everybody in, otherwise authenticate
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                // -> spring : don't bother creating a session, don't manage sessions
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
